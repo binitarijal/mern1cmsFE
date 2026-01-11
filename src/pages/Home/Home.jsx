f688b848401083f5b480bb1425bfde72e5708ab7
Binita rijal <rijalbinita01@gmail.com>
2026-01-11 15:12:41 +0545

    get blog api hit , cors, home ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,47 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navebar'
+import axios from 'axios'
+import { use } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 
 const Home = () => {
+
+    const[blogs,setBlogs]=useState([])
+const fetchBlogs=async ()=>{
+    const response= await axios.get('http://localhost:3000/blogs')
+    console.log(response.data.blogs);
+    setBlogs(response.data.blogs)
+}
+useEffect(()=>{
+    fetchBlogs()
+},[])
+
   return (
     <>
     <Navbar/>
-    <div class="card">
-  <div class="card-content">
-    <h3 class="card-title">Future of Web Design</h3>
-    <p class="card-text">
-      Explore the latest trends in UI/UX for 2026, including glassmorphism, 
-      dynamic micro-interactions, and AI-driven layouts.
+    {
+ blogs.map((blog)=>{
+    return(
+        <div className="card" key={blog._id}>
+  
+  <div className="card-content">
+    <h3 className="card-title">{blog.title} </h3>
+    <p className="card-text">
+    {blog.subTitle}
     </p>
-    <a href="#" class="card-link">
+    <a href="/singleBlog" className="card-link">
       See More 
-      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
     </a>
   </div>
 </div>
+    )
 
+ })
+}
     </>
   )
 }
